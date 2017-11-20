8d6a45ba265ebe68ce3c9294fd45365da3569d25
Rien Cosyns <rien.cosyns@gmail.com>
2017-11-20 14:41:10 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
+  post '/signup', to: 'users#create'
+
   # get 'static_pages/home'
 
   get '/help', to: 'static_pages#help'
@@ -11,4 +13,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  resources :users
 end
